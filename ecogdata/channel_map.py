351a734263e6b67944e983a9155832756fca4ec3
miketrumpis <mtrumpis@gmail.com>
2019-07-19 15:44:38 -0700

    syntax cleanup

--- ecogdata/channel_map.py
+++ ecogdata/channel_map.py
@@ -163,11 +163,10 @@ class ChannelMap(list):
             super(ChannelMap, self).__getitem__(index),
             self.geometry, col_major=self.col_major, pitch=self.pitch
             )
-        import sys
         # Keep the pre-computed combinations IFF the entire map is copied
         i = index.start if index.start else 0
         j = index.stop if index.stop else len(self)
-        if i==0 and j==len(self) and self._combs is not None:
+        if i == 0 and j == len(self) and self._combs is not None:
             new_map._combs = self._combs.copy()
         return new_map
 
