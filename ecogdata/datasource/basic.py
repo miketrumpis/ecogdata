8387903404bc25352ae28c93236e2422e8db7429
miketrumpis <mtrumpis@gmail.com>
2019-08-12 12:06:01 -0700

    expose an aggregating downsample option (best for TTL signals)

--- ecogdata/datasource/basic.py
+++ ecogdata/datasource/basic.py
@@ -1,3 +1,4 @@
+import numpy as np
 from tqdm import tqdm
 
 from ecogdata.expconfig import load_params
@@ -148,7 +149,32 @@ class ElectrodeDataSource(object):
                 chans_per_block = 16
         return DataSourceBlockIter(self, axis=0, block_length=chans_per_block, return_slice=return_slice)
 
-    def batch_change_rate(self, new_rate_ratio, new_source, antialias_aux=False, verbose=False, filter_inplace=False):
+    def batch_change_rate(self, new_rate_ratio, new_source, antialias_aligned=False, aggregate_aligned=True,
+                          verbose=False, filter_inplace=False):
+        """
+        Downsample a datasource into a new source. Preserves timing alignment of any aligned arrays.
+
+        Parameters
+        ----------
+        new_rate_ratio: int
+            The downsample rate: old_rate / new_rate
+        new_source: ElectrodeDataSource
+            A Datasource with the correct channel layouts
+        antialias_aligned: bool
+            Downsample aligned channels with anti-aliasing (may distort logic level signals). If False,
+            then just decimate.
+        aggregate_aligned: bool
+            Reduce sampling rate by accumulate samples in aligned channels. Good choice to logic level, and provides
+            some antialiasing.
+        verbose: bool
+            Use a progress bar.
+        filter_inplace: bool
+            Do anti-aliasing filtering in-place. TODO: should be set to True for mapped sources
+
+        Returns
+        -------
+
+        """
         new_rate_ratio = int(new_rate_ratio)
         if new_source.shape[0] != self.shape[0]:
             raise ValueError('Output source has the wrong number of channels: {}'.format(new_source.shape[0]))
@@ -163,18 +189,30 @@ class ElectrodeDataSource(object):
             r = downsample(raw_channels, float(new_rate_ratio), r=new_rate_ratio, filter_inplace=filter_inplace)[0]
             new_source[sl] = r
 
-        # now decimate aux_channels with or without anti-aliasing -- *assuming* that a full load won't bust RAM
+        # now decimate aligned_channels with or without anti-aliasing -- *assuming* that a full load won't bust RAM
         for k_src, k_dst in zip(self.aligned_arrays, new_source.aligned_arrays):
             print('Downsampling array {}-->{}'.format(k_src, k_dst))
-            a_src = getattr(self, k_src)
+            a_src = getattr(self, k_src)[:, :]
+            if self._transpose:
+                # do copy b/c downsample would need c-contiguous
+                a_src = a_src.T.copy()
             a_dst = getattr(new_source, k_dst)
-            if antialias_aux:
-                a_dst[:, :] = downsample(a_src[:, :], float(new_rate_ratio), r=new_rate_ratio,
+            if antialias_aligned:
+                a_dst[:, :] = downsample(a_src, float(new_rate_ratio), r=new_rate_ratio,
                                          filter_inplace=filter_inplace)[0]
+            elif aggregate_aligned:
+                T = new_rate_ratio * a_dst.shape[1]
+                if T > a_src.shape[1]:
+                    T -= new_rate_ratio
+                a_src = a_src[:, :T].reshape(a_dst.shape[0], T // new_rate_ratio, -1).sum(axis=2)
+                if a_src.shape[1] < a_dst.shape[1]:
+                    a_src = np.c_[a_src, np.zeros(len(a_src))]
+                    assert a_src.shape == a_dst.shape
+                a_dst[:, :] = a_src
             else:
                 # make some redundant slicing in case this is a mapped array:
                 # strided reads from HDF5 are horribly slow!
-                a_dst[:, :] = a_src[:, :][:, ::new_rate_ratio]
+                a_dst[:, :] = a_src[:, ::new_rate_ratio]
 
     def filter_array(self, **kwargs):
         # Needs overload
@@ -193,7 +231,7 @@ class PlainArraySource(ElectrodeDataSource):
 
     """
 
-    def __init__(self, data_matrix, use_shared_mem=False, **aux_arrays):
+    def __init__(self, data_matrix, use_shared_mem=False, **aligned_arrays):
         """
 
         Parameters
@@ -204,7 +242,7 @@ class PlainArraySource(ElectrodeDataSource):
             Sampling rate S/s
         use_shared_mem: bool
             If True, copy arrays to shared memory. Wasteful if arrays are already shared.
-        aux_arrays: dict
+        aligned_arrays: dict
             Any other datasets that should be aligned with the electrode signal array. These arrays
             will be kept at the same sampling rate and index alignment as the electrode signal. They will also be
             preserved if this source is mirrored or copied to another source.
@@ -213,9 +251,9 @@ class PlainArraySource(ElectrodeDataSource):
         self._data_matrix = shared_copy(data_matrix) if use_shared_mem else data_matrix
         self.shape = data_matrix.shape
         self.dtype = data_matrix.dtype
-        for name in aux_arrays:
-            setattr(self, name, (shared_copy(aux_arrays[name]) if use_shared_mem else aux_arrays[name]))
-        self.aligned_arrays = list(aux_arrays.keys())
+        for name in aligned_arrays:
+            setattr(self, name, (shared_copy(aligned_arrays[name]) if use_shared_mem else aligned_arrays[name]))
+        self.aligned_arrays = list(aligned_arrays.keys())
 
     def __getitem__(self, slicer):
         return self._data_matrix[slicer]
@@ -243,9 +281,9 @@ class PlainArraySource(ElectrodeDataSource):
         f_arr = filter_array(self._data_matrix, **kwargs)
         if inplace:
             return self
-        aux_arrays = dict([(a, getattr(self, a)) for a in self.aligned_arrays])
+        aligned_arrays = dict([(a, getattr(self, a)) for a in self.aligned_arrays])
         # use_shared_mem = False b/c the array returned from filter_array is already shared mem.
-        return PlainArraySource(f_arr, use_shared_mem=False, **aux_arrays)
+        return PlainArraySource(f_arr, use_shared_mem=False, **aligned_arrays)
 
     def notch_filter(self, **kwargs):
         """
@@ -267,8 +305,8 @@ class PlainArraySource(ElectrodeDataSource):
         f_arr = notch_all(self._data_matrix, **kwargs)
         if inplace:
             return self
-        aux_arrays = dict([(a, getattr(self, a)) for a in self.aligned_arrays])
+        aligned_arrays = dict([(a, getattr(self, a)) for a in self.aligned_arrays])
         # use_shared_mem = False b/c the array returned from filter_array is already shared mem.
-        return PlainArraySource(f_arr, use_shared_mem=False, **aux_arrays)
+        return PlainArraySource(f_arr, use_shared_mem=False, **aligned_arrays)
 
 
