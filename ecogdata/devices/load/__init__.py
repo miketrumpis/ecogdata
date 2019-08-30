89edf3746f370b3422534894272339e610b2a1b2
miketrumpis <mtrumpis@gmail.com>
2019-08-30 10:46:24 -0700

    modernize

--- ecogdata/devices/load/__init__.py
+++ ecogdata/devices/load/__init__.py
@@ -1,14 +1,12 @@
 """
-This module should eventually house device specific code, e.g.
-
-* current sensing AFE headstage
-* mux Vx headstages
-* blackrock headstage
-
+This package implements data loading/mapping for various acquisition systems. Common logic for loading &
+preprocessing is implemented in the file2data.FileLoader class.
 """
+
 
 class DataPathError(Exception):
     pass
+
 
 from .mux import load_mux, mux_headstages
 from .blackrock import load_blackrock
@@ -19,3 +17,5 @@ from .active_electrodes import load_active, active_headstages
 from .open_ephys import load_open_ephys
 from .nanoz import *
 from .intan import *
+
+
