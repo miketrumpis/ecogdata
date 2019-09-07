a36a7ec96e82bafb408e734f73c26c0cc083b5c9
miketrumpis <mtrumpis@gmail.com>
2019-09-06 17:19:26 -0700

    string types do not get turned into normal arrays

--- ecogdata/datastore/h5utils.py
+++ ecogdata/datastore/h5utils.py
@@ -11,7 +11,7 @@ from ecogdata.util import Bunch
 from ecogdata.parallel.array_split import shared_ndarray
 
 
-_h5_seq_types = (str, type(1.0), type(1j), type(True), type([]))
+_h5_seq_types = (type(1.0), type(1j), type(True), type([]))
 
 
 class HDF5Bunch(Bunch):
