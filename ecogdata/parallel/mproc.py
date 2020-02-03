a16bebfddfefe173d4699fe437b8207db04ec7e0
miketrumpis <mtrumpis@gmail.com>
2020-02-03 13:34:11 -0800

    context-full logger for multiproc

--- ecogdata/parallel/mproc.py
+++ ecogdata/parallel/mproc.py
@@ -3,10 +3,43 @@
 import numexpr
 numexpr.set_num_threads(1)
 
+from contextlib import contextmanager
 import platform
 from multiprocessing import *
 import multiprocessing.sharedctypes
 sharedctypes = multiprocessing.sharedctypes
+
+
+_stderr_logger = None
+
+
+@contextmanager
+def make_stderr_logger(level='info'):
+    # if None, then use "NOTSET" level
+    import logging
+    # log_to_stderr() should already prevent duplicate loggers, but it *does* add subsequent handlers
+    global _stderr_logger
+    if _stderr_logger is None:
+        _stderr_logger = log_to_stderr()
+    logger = _stderr_logger
+    logger.propagate = False
+    base_level = logger.level
+    if level is None:
+        logger.setLevel(logging.NOTSET)
+    elif level.lower() == 'info':
+        logger.setLevel(logging.INFO)
+    elif level.lower() in ('warn', 'warning'):
+        logger.setLevel(logging.WARNING)
+    elif level.lower() == 'error':
+        logger.setLevel(logging.ERROR)
+    elif level.lower() == 'critical':
+        logger.setLevel(logging.CRITICAL)
+
+    try:
+        yield logger
+    finally:
+        logger.setLevel(base_level)
+
 
 if platform.system().lower().find('windows') < 0:
     # Do monkey-patch suggested for Python Issue 30919
