775d8397f53e2e742068705b77d9df7ee2ef0664
miketrumpis <mtrumpis@gmail.com>
2021-09-28 16:51:45 -0700

    another job, plus fix cibuild

---
+++ ecogdata/__init__.py
@@ -0,0 +1,12 @@
+from . import datasource
+from . import datastore
+from . import devices
+from . import expconfig
+from . import filt
+from . import parallel
+from . import channel_map
+from . import trigger_fun
+from . import util
+
+
+__version__ = '0.1.1'
