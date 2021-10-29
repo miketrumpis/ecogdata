534befa86b8d865668e879e29f2ce9cd1a709682
miketrumpis <mtrumpis@gmail.com>
2021-10-29 13:41:03 -0700

    newer setuptools + pep317,318 rules.. simplify tox.ini

--- setup.py
+++ setup.py
@@ -1,41 +1,6 @@
-import os
-from glob import glob
-from setuptools import setup, Extension, find_packages
-from numpy.distutils.command import build_src
-# import Cython.Compiler.Main
-# build_src.Pyrex = Cython
-# build_src.have_pyrex = True
-# from Cython.Distutils import build_ext
-from Cython.Build import cythonize
-import numpy
+#!/usr/bin/env python
 
-try:
-    from numpy.distutils.misc_util import get_numpy_include_dirs
-    numpy_include_dirs = get_numpy_include_dirs()
-except AttributeError:
-    numpy_include_dirs = numpy.get_include()
+import setuptools
 
-
-dirs = list(numpy_include_dirs)
-
-
-# slepian_projection = Extension(
-#     'ecogdata.filt.time._slepian_projection',
-#     ['src/ecogdata/filt/time/_slepian_projection.pyx'],
-#     include_dirs = dirs,
-#     libraries=(['m'] if os.name != 'nt' else []),
-#     extra_compile_args=['-O3']
-#     )
-
-
-if __name__=='__main__':
-    setup(
-        name='ecogdata',
-        version='0.1',
-        package_dir={'': 'src'},
-        packages=find_packages(where='src'),
-        scripts=glob('scripts/*.py'),
-        # ext_modules=cythonize([slepian_projection]),
-        # cmdclass={'build_ext': build_ext},
-        package_data={'ecogdata.expconfig': ['*.txt']}
-    )
+if __name__ == "__main__":
+    setuptools.setup()
